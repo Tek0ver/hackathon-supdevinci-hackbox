634fb7973008d8fec307df308a30e561b2e60fd9
Tek0ver <thibault.foure@outlook.com>
2023-07-05 16:24:55 +0200

    add test mode

--- hackbox/list_tests.py
+++ hackbox/list_tests.py
@@ -2,36 +2,43 @@ from scripts.foo_ip_collect import foo_ip_collect
 from scripts.foo_scan_open_ports import foo_scan_open_ports
 from scripts.foo_count import foo_count
 
-def test3():
-    print("Voilà les CVE !")
+test_mode = True
 
-tests = [
-    {
-        "id": 1,
-        "title": "Récupération des ips",
-        "description": "",
-        "command": foo_ip_collect,
-        "parameter": []
-    },
-    {
-        "id": 2,
-        "title": "Scan des ports",
-        "description": "",
-        "command": foo_scan_open_ports,
-        "parameter": []
-    },
-    {
-        "id": 3,
-        "title": "Recherche des CVE",
-        "description": "",
-        "command": test3,
-        "parameter": []
-    },
-    {
-        "id": 4,
-        "title": "Counter(for testing)",
-        "description": "Lance un compteur pour tester.",
-        "command": foo_count,
-        "parameter": [('tick', 'int', 2), ('max', 'int', 30)]
-    }
-]
+if not test_mode:
+    pass
+
+else:
+
+    def test3():
+        print("Voilà les CVE !")
+
+    tests = [
+        {
+            "id": 1,
+            "title": "Récupération des ips",
+            "description": "",
+            "command": foo_ip_collect,
+            "parameter": []
+        },
+        {
+            "id": 2,
+            "title": "Scan des ports",
+            "description": "",
+            "command": foo_scan_open_ports,
+            "parameter": []
+        },
+        {
+            "id": 3,
+            "title": "Recherche des CVE",
+            "description": "",
+            "command": test3,
+            "parameter": []
+        },
+        {
+            "id": 4,
+            "title": "Counter(for testing)",
+            "description": "Lance un compteur pour tester.",
+            "command": foo_count,
+            "parameter": [('tick', 'int', 2), ('max', 'int', 30)]
+        }
+    ]
